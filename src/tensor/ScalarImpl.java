0775a779a284d4fa3d86bc7e21d3d255ec5fe8ef
issac <0712wn@gmail.com>
2025-05-26 13:50:39 +0900

    ScalarIMpl done

--- src/tensor/ScalarImpl.java
+++ src/tensor/ScalarImpl.java
@@ -2,7 +2,7 @@ package tensor;
 
 import java.math.BigDecimal;
 
-public class ScalarImpl implements Scalar,Cloneable {
+class ScalarImpl implements Scalar,Cloneable {
     //스칼라의 자료구조
     BigDecimal scalarValue = null;
 
@@ -18,6 +18,43 @@ public class ScalarImpl implements Scalar,Cloneable {
         //스칼라 생성
     }
 
+    //12. 스칼라의 값 지정
+    public void setScalar(String value)
+    {
+        System.out.println("[val]로 값 지정");
+    }
+
+    //12. 스칼라의 값 조회
+    public String getScalar()
+    {
+        String result = "TEST";
+        System.out.println("스칼라 값 얻음");
+        return result;
+    }
+
+    //14s. 스칼라 객체 콘솔에 출력
+    @Override
+    public String toString()
+    {
+        String result = "TEST";
+        return result;
+    }
+
+    //15s. 객체의 동등성 판단
+    @Override
+    public boolean equals(Object o)
+    {
+        boolean result = false;
+        return false;
+    }
+
+    //16 스칼라의 값 대 소 비교
+    public boolean valueCompare(Scalar A,Scalar B)
+    {
+        boolean result = false;
+        return false;
+    }
+
     //17s 객체 복제(Impl에서 override)
     public Scalar cloneSelf()
     {
@@ -26,7 +63,21 @@ public class ScalarImpl implements Scalar,Cloneable {
     @Override
     protected Scalar clone()
     {
-        return null;
+        Scalar result = null;
+        System.out.println("객체 복제됨");
+        return result;
+    }
+
+    //18 다른 스칼라와 덧셈
+    public void addScalar(Scalar val)
+    {
+        System.out.println("[val] 더해짐");
+    }
+
+    //19 다른 스칼라와 곱셈
+    public void multiplyScalar(Scalar val)
+    {
+        System.out.println("[val] 곱해짐");
     }
 
     //24 전달 받은 두 스칼라의 덧셈
